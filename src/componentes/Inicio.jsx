e5db1742105f6abd06c7987ad8f67b7c3f5cc4a1
mew <102923191+meeeww@users.noreply.github.com>
2023-04-18 02:11:22 +0200

    Mejoras en inicio y terminado HTML. Comienzo css

--- src/componentes/Inicio.jsx
+++ src/componentes/Inicio.jsx
@@ -1,8 +1,8 @@
-
+import bellumVideo from '../assets/VideoBellum.mp4'
 import bellumLogo from '../assets/Logo.png'
 
 const Inicio = () => (
-    <div>
+    <div className="bodyBellum">
         <div className="titulo">
             <div className="tituloIzquierda">
                 <h1>Bienvenidos a Bellum Coaching</h1>
@@ -19,7 +19,7 @@ const Inicio = () => (
 
         <div className="subtitulo">
             <div className="subtituloIzquierda">
-                <img src={bellumLogo} alt="Bellum Logo" />
+                <video src={bellumVideo} alt="Bellum Video" autoPlay loop muted="muted" controlsList="nodownload"></video>
             </div>
             <div className="subtituloDerecha">
                 <h2>Somos Bellum</h2>
@@ -30,7 +30,7 @@ const Inicio = () => (
                     técnicas y trabajo en equipo. Ya seas principiante o jugador experimentado, nuestro coaching para
                     League of Legends totalmente personalizado se ajusta a tus necesidades para ayudarte a alcanzar tu
                     máximo potencial. ¡Contáctanos hoy mismo para comenzar a lograr tus objetivos en la grieta!</h4>
-                    <button>CONTÁCTANOS</button>
+                <button>CONTÁCTANOS</button>
             </div>
         </div>
 
@@ -67,10 +67,43 @@ const Inicio = () => (
 
         <div className="clientesPiensan">
             <h2>Lo Que Nuestros Clientes Piensan</h2>
+            <div className="tarjetaTrustpilot">
+                <p>Desde Bellum Coaching creemos que lo más importante es estar abiertos a críticas constructivas para poder mejorar nuestro servicio.</p>
+                <div className="trustpilot-widget" data-locale="es-ES" data-template-id="5419b6a8b0d04a076446a9ad" data-businessunit-id="6436c8f8337ac0fb935bbea5" data-style-height="24px" data-style-width="100%" data-theme="light" data-min-review-count="10" data-style-alignment="center">
+                    <a href="https://es.trustpilot.com/review/www.bellumcoaching.com" target="_blank" rel="noopener">Trustpilot</a>
+                </div>
+            </div>
+
         </div>
 
         <div className="precios">
-            <h2>Nuestros Precios</h2>
+            <h2>Nuestros Precios Para Clases Individuales</h2>
+            <div className="tarjetasPrecios">
+                <div className="tarjetaPrecios" id="mejorParaEmpezar">
+                    <h4>1 Clase Individual</h4>
+                    <h6>5€</h6>
+                </div>
+                <div className="tarjetaPrecios">
+                    <h4>3 Clases Individuales</h4>
+                    <h6>13€</h6>
+                </div>
+                <div className="tarjetaPrecios" id="masVendido">
+                    <h4>5 Clases Individuales</h4>
+                    <h6>20€</h6>
+                </div>
+                <div className="tarjetaPrecios">
+                    <h4>10 Clases Individuales</h4>
+                    <h6>40€</h6>
+                </div>
+                <div className="tarjetaPrecios" id="masRentable">
+                    <h4>20 Clases Individuales</h4>
+                    <h6>70€</h6>
+                </div>
+                <div className="tarjetaPrecios">
+                    <h4>20 Clases Individuales</h4>
+                    <h6>70€</h6>
+                </div>
+            </div>
         </div>
 
         <div className="decision">
@@ -79,7 +112,27 @@ const Inicio = () => (
             <div className="tarjetasDecision">
                 <div className="tarjetaDecision">
                     <h4 id="h4">Experiencia y conocimiento</h4>
-                    <p>Contamos</p>
+                    <p>Contamos con un equipo de entrenadores altamente capacitados y actualizados en las últimas tendencias y estrategias del juego. Nuestro objetivo es brindarte un servicio de coaching excepcional, y para lograrlo nos aseguramos de estar siempre a la vanguardia del juego. Ya sea que necesites mejorar tu juego individual o trabajar en equipo, nuestro equipo de entrenadores está listo para ayudarte. Contáctanos hoy mismo para obtener más información.</p>
+                </div>
+                <div className="tarjetaDecision">
+                    <h4 id="h4">Formalidad, compromiso y responsabilidad.</h4>
+                    <p>Estamos comprometidos con brindar un servicio de alta calidad en cada sesión de coaching. Nuestro objetivo es ayudar a nuestros clientes a alcanzar su máximo potencial con profesionalismo y dedicación. Nos aseguramos de que cada sesión sea personalizada y adaptada a las necesidades de cada jugador. Si buscas mejorar tus habilidades y estrategias en el juego, nuestro equipo de entrenadores está aquí para ayudarte.</p>
+                </div>
+                <div className="tarjetaDecision">
+                    <h4 id="h4">Empatía, cercanía y amabilidad.</h4>
+                    <p>Establecemos relaciones duraderas con nuestros clientes y brindamos apoyo continuo. Escuchamos sus inquietudes y respondemos a sus preguntas para asegurarnos de que estén completamente satisfechos con nuestros servicios. Si estás buscando un equipo de entrenadores dedicados a ayudarte a mejorar tus habilidades y estrategias en el juego, no dudes en contactarnos. Estamos aquí para ayudarte a alcanzar tus metas.</p>
+                </div>
+                <div className="tarjetaDecision">
+                    <h4 id="h4">Retroalimentación constante.</h4>
+                    <p>Brindamos retroalimentación constante y constructiva a nuestros clientes durante las sesiones de coaching. Identificamos sus fortalezas y debilidades específicas y los ayudamos a mejorar en esas áreas. Si estás buscando un equipo de entrenadores dedicados a ayudarte a mejorar tus habilidades y estrategias en el juego, no dudes en contactarnos. Estamos aquí para ayudarte a alcanzar tus metas.</p>
+                </div>
+                <div className="tarjetaDecision">
+                    <h4 id="h4">Enfoque en el progreso a largo plazo.</h4>
+                    <p>Nos enfocamos en ayudar a nuestros clientes a alcanzar objetivos a largo plazo en el juego. Ofrecemos entrenamiento constante y seguimiento para mejorar continuamente sus habilidades y estrategias. No ofrecemos soluciones rápidas a problemas específicos, sino que nos dedicamos a mejorar de manera constante. Si estás buscando un servicio de coaching personalizado, ¡contáctanos hoy mismo!</p>
+                </div>
+                <div className="tarjetaDecision">
+                    <h4 id="h4">Adaptación individual para cada cliente.</h4>
+                    <p>Nos centramos en brindar servicios de coaching personalizados para satisfacer las necesidades únicas de cada cliente. Trabajamos en estrecha colaboración con nuestros clientes para identificar sus fortalezas y debilidades individuales y proporcionar un plan de coaching adaptado a sus objetivos y habilidades específicas. ¡Nuestro objetivo es ayudar a nuestros clientes a alcanzar el éxito a largo plazo!</p>
                 </div>
             </div>
         </div>
