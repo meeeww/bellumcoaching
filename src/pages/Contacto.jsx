757a87fd3bda33c9656bd2f2a9daed5f666d2212
mew <102923191+meeeww@users.noreply.github.com>
2023-04-24 21:14:41 +0200

    editing contacto

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,14 +1,30 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import { Link } from "react-router-dom"
 import MainLayout from "../layout/MainLayout";
 import bellumVideo from '../assets/VideoBellum.mp4'
 import bellumVideoLight from '../assets/VideoBellumLight.mp4'
 import bellumLogo from '../assets/Logo.png'
+import Axios from 'axios'
 
 const Contacto = () => {
 
     const nombrePagina = "Contacto"
+
+    const [nombreContacto, setNombreContacto] = useState('')
+    const [apellidoContacto, setApellidoContacto] = useState('')
+    const [correoContacto, setCorreoContacto] = useState('')
+    const [asuntoContacto, setAsuntoContacto] = useState('')
+    const [mensajeContacto, setMensajeContacto] = useState('')
+
+    const enviarMensajeContacto = (event) => {
+        event.preventDefault();
+
+        Axios.post("https://bellumserver.netlify.app/.netlify/functions/api/users", { nombreContacto: nombreContacto, apellidoContacto: apellidoContacto, correoContacto: correoContacto, asuntoContacto: asuntoContacto, mensajeContacto: mensajeContacto }).then(() => {
+            alert("bien")
+            setPosts([nombreContacto, res.data])
+        })
+    }
 
     return (
         <MainLayout laPagina={nombrePagina}>
@@ -23,22 +39,22 @@ const Contacto = () => {
                         <h4 id="h4">Utilizamos Twitter, Instagram y Discord. ¡No dudes en ponerte en contacto con nosotros por tu medio favorito!</h4>
                     </div>
                     <div className="tituloDerechaContacto">
-                        <form netlify>
+                        <form>
                             <div className="juntarContacto">
-                                <input type="text" placeholder="Nombre"></input>
-                                <input type="text" placeholder="Apellido"></input>
+                                <input type="text" placeholder="Nombre" onChange={(e) => { setNombreContacto(e.target.value) }}></input>
+                                <input type="text" placeholder="Apellido" onChange={(e) => { setApellidoContacto(e.target.value) }}></input>
                             </div>
                             <div className="juntarContacto">
-                                <input type="text" placeholder="Correo Electrónico"></input>
+                                <input type="text" placeholder="Correo Electrónico" onChange={(e) => { setCorreoContacto(e.target.value) }}></input>
                             </div>
                             <div className="juntarContacto">
-                                <input type="text" placeholder="Asunto"></input>
+                                <input type="text" placeholder="Asunto" onChange={(e) => { setAsuntoContacto(e.target.value) }}></input>
                             </div>
                             <div className="juntarContacto">
-                                <input type="text" placeholder="Tu Mensaje" className="mensajeContacto"></input>
+                                <input type="text" placeholder="Tu Mensaje" className="mensajeContacto" onChange={(e) => { setMensajeContacto(e.target.value) }}></input>
                             </div>
                             <div className="juntarContacto">
-                                <input type="submit" value="Submit"></input>
+                                <input type="submit" value="Submit" onClick={enviarMensajeContacto}></input>
                             </div>
                         </form>
                     </div>
