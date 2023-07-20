33faef8eeeb7131f3780cf984e23991878289a22
Rakions <ogmohammed2004@hotmail.com>
2023-07-20 23:27:28 +0200

    Cambios modal

--- src/pages/Dashboard/admin/ModalInfo.jsx
+++ src/pages/Dashboard/admin/ModalInfo.jsx
@@ -2,7 +2,7 @@ const ModalInfo = () => {
     return (
         <>
             <div className="modalInfo w-full h-screen bg-[#1118277e] fixed top-0 left-0 flex items-center justify-center p-4">
-                <div className="h-[250px] md:w-[350px] w-full bg-[var(--color-principal-dashboard)]">
+                <div className="h-[270px] md:w-[350px] w-full bg-[var(--color-principal-dashboard)]">
                     <div className="w-full h-[40px] bg-[var(--color-fondo-dashboard)] flex items-center justify-between px-4">
                         <div><p className="text-white text-xl">Información</p></div>
                         <button className="h-full">X</button>
@@ -20,7 +20,7 @@ const ModalInfo = () => {
                             <p className="text-white">Contraseña: </p>
                             <input type="password" placeholder="nombre" className="w-[150px] text-[1rem] pl-1 bg-[var(--color-principal-dashboard)] text-[var(--color-texto-dashboard)]" />
                         </div>
-                        <button className="bg-[var(--color-texto)]">Confirmar</button>
+                        <button className="bg-[var(--color-texto-dashboard)] rounded-md mt-8">Confirmar</button>
                     </div>
                 </div>
             </div>
