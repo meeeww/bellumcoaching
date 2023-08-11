231f1526b4e3067ba9272ec8e1507f9bfe0860b7
meeeww <thezasm03@gmail.com>
2023-08-11 23:43:52 +0200

    Debug 6

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -7,7 +7,7 @@ import { Toaster, toast } from 'sonner'
 import { Webhook, MessageBuilder } from 'discord-webhook-node';
 const hook = new Webhook("https://discord.com/api/webhooks/1139668043153297500/pQBAiMRWXVp3GSUE1Z4aTWIiOSVkpg-xA2hPbPdu-OdMmZgsXZP8AmMSzlRnNsHIPqDt");
 
-const API = "RGAPI-9c89909e-770b-4acf-aaa6-9eb07f095247"
+const API = "RGAPI-9c89909e-770b-4acf-aaa6-9eb07f095247" //
 
 const Contacto = () => {
     const [nombreContacto, setNombreContacto] = useState('')
